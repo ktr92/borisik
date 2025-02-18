5043dd62dfe04e042a1c2f50a9224016cec9a0c8
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-02-18 20:22:16 +0300

    fix

--- dist/js/filter.js
+++ dist/js/filter.js
@@ -10,23 +10,28 @@ window.onload = function () {
 
     $('input#maxCost1').on('focusin', function () {
       $(this).val("");
+      $(this).trigger('change')
     });
 
     $('input#minCost1').on('focusin', function () {
       $(this).val("");
+      $(this).trigger('change')
     });
     $('input#maxCost2').on('focusin', function () {
       $(this).val("");
+      $(this).trigger('change')
     });
 
     $('input#minCost2').on('focusin', function () {
       $(this).val("");
+      $(this).trigger('change')
     });
 
 
     $("#maxCost1").val($('#limitMax1').val());
     $("#minCost1").val($('#limitMin1').val());
-
+    $("#maxCost1").trigger('change')
+    $("#minCost1").trigger('change')
     $("#maxCost1").prop("placeholder", $("#limitMax1").val());
     $("#minCost1").prop("placeholder", $("#limitMin1").val());
 
@@ -42,6 +47,7 @@ window.onload = function () {
         stop: function (event, ui) {
           jQuery("input#minCost1").val(jQuery("#sliderprice1").slider("values", 0));
           jQuery("input#maxCost1").val(jQuery("#sliderprice1").slider("values", 1));
+        
 
           //var val1 = $test1.prop("value");
           //$test1.prop("value", prettify(val1));
@@ -76,7 +82,8 @@ window.onload = function () {
           } else {
             jQuery("input#maxCost1").removeClass('notchanged');
           }
-
+          jQuery("input#maxCost1").trigger('change')
+          jQuery("input#minCost1").trigger('change')
 
           //var val1 = $test1.prop("value");
           // $test1.prop("value", prettify(val1));
@@ -124,6 +131,7 @@ window.onload = function () {
         if (value2 < (+jQuery("input#limitMin1").val())) {
           value2 = +jQuery("input#limitMin1").val();
           jQuery("input#minCost1").val(+jQuery("input#limitMin1").val())
+          jQuery("input#minCost1").trigger('change')
         }
 
 
